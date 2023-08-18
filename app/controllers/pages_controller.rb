fcee69cccdad913e080bfe3600b2f1f1e1dad6d9
mguan2020 <mguan988@ucla.edu>
2023-08-18 09:50:03 -0700

    Add existing project files to Git

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+    def home
+       
+    end
+end
+
+
+// binary search, search for the length of the pieces we're going to make through cutting
+// guess a length len, check if we can make at least k pieces of length len by cutting
+// search range of len is 1 to maximum value in wood
+// find the biggest len such that we can make at least pieces
+// TC: O(log(max val in woods) * len(woods))
+
+public int maxLength(int[] wood, int k){
+int lo 
+}
+
+public boolean atLeastK(int[] wood, int k, int len){
+
+}
+
+
